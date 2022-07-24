600aca7c76fa5766f9c096c8015c8794a1423444
Justin Evans <eustin.jvans@gmail.com>
2022-07-25 09:00:08 +1000

    create initial script and README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Go Python!
+
+Bootstrapping my Python projects by following the same steps each time sucks! 
+
+This script creates a new project directory, installs a Pipenv, initialises a git repo, and creates some commonly used files and folders to speed up Python development. 
+
+## Requirements
+
+* Bash
+* Pipenv
+* Git
+
+## How to install
+
+TBC
---
+++ go-python.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+printf "=== Go Python! ===\n\n"
+
+# check if Pipenv in installed
+if ! command -v pipenv &> /dev/null 
+then 
+    printf "Please install Pipenv before proceeding\n"
+    exit
+fi
+
+if ! command -v git &> /dev/null
+then
+    printf "Please install git before proceeding\n"
+    exit
+fi
+
+printf "What do you want your project directory to be called?\n"
+read project_name
+mkdir $project_name
+printf "created $PWD/$project_name\n"
+
+cd $project_name
+git init
+pipenv install
+pipenv run pipenv install --dev pytest 
+
+# create files and folders commonly used in python projects
+touch main.py
+printf "**/.env\n" > .gitignore
+printf "**/__pychache__" >> .gitignore
+mkdir src tests
+
+printf "Dropping you into your Pipenv. Happy Python-ing!"
+pipenv shell
